453098a5db38f25779eea3a6a7989071d61dd07e
Oleg Lavrovsky <oleg@utou.ch>
2012-08-14 13:57:23 +0200

    Aare temperatures widget

---
+++ services/hydrodaten.php
@@ -0,0 +1,37 @@
+<?php
+
+// remote URL to proxy
+$url = 'http://www.hydrodaten.admin.ch/lhg/SMS.xml';
+
+// cache file
+$file = "hydrodaten.xml";
+
+// cache timeout in seconds
+$ttl = 46400; // twice a day
+
+// refresh the cache
+if (!is_file($file) || filemtime($file)<time()-(86400*30))
+{
+  $ch = curl_init($url);
+  $fp = fopen($file, "w");
+  curl_setopt($ch, CURLOPT_FILE, $fp);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_exec($ch);
+  curl_close($ch);
+  fflush($fp);
+  fclose($fp);
+}
+
+$exp_gmt = gmdate("D, d M Y H:i:s", time() + $ttl * 60) ." GMT";
+$mod_gmt = gmdate("D, d M Y H:i:s", filemtime($file)) ." GMT";
+header("Expires: " . $exp_gmt);
+header("Last-Modified: " . $mod_gmt);
+header("Cache-Control: public, max-age=" . $ttl * 60);
+// for MSIE 5
+header("Cache-Control: pre-check=" . $ttl * 60, FALSE);  
+header ('Content-Type: text/xml');
+
+// push the cache out
+readfile($file);
+
+?>
