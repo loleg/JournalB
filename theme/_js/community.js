943532194a57a22b9a09ac3aade8f740db808a04
Oleg Lavrovsky <oleg@utou.ch>
2012-11-20 07:30:19 +0100

    One ad, not two in community

--- theme/_js/community.js
+++ theme/_js/community.js
@@ -48,50 +48,25 @@ function scaleCommunity()
 	});
 }
 
-// Draw two random ads on every page, and cycle them
+// Draw random ads on every page, and cycle them
 function drawAds()
 {
 	var community_count = $(".sidebar .content .community-item").length;
-	var pos1 = Math.floor((Math.random()*community_count));
-	var pos2 = Math.floor((Math.random()*community_count));
-	if (community_count>1) while (pos2==pos1) pos2 = Math.floor((Math.random()*community_count));
-	
-	var ad1=0,ad2=0;
-		
+	var pos1 = Math.floor(Math.random()*community_count);
 	var ads_count = $(".community-ads .adbox").length;
-	if (ads_count==0) return;
-	else if (ads_count==1) ad1 = 1;
-	else if (ads_count==2)
-	{
+	var current_ad_shift = getCookie("adShift");
+	if (!current_ad_shift) current_ad_shift = 0; else current_ad_shift = parseInt(current_ad_shift);
+	var ad1 = current_ad_shift + 1;
+	if (ad1>ads_count) {
 		ad1 = 1;
-		ad2 = 2;
+		current_ad_shift = 0;
 	}
-	else
-	{
-		var current_ad_shift = getCookie("adShift");
-		if (!current_ad_shift) current_ad_shift = 0; else current_ad_shift = parseInt(current_ad_shift);
-		ad1 = current_ad_shift + 1;
-		ad2 = current_ad_shift + 2;
-		if (ad1>ads_count)
-		{
-			ad1 = 1;
-			ad2 = 2;
-			current_ad_shift = 0;
-		}
-		else if (ad2>ads_count)
-		{
-			ad2 = 1;
-		}
-		
-		current_ad_shift = ad2;
-		setCookie("adShift",current_ad_shift,7);
-	}
-			
+	current_ad_shift = ad1;
+	setCookie("adShift",current_ad_shift,7);		
 	if (ad1) $(".community-ads .adbox:nth-child("+ad1+")").insertAfter($(".sidebar .content .community-item")[pos1]);
-	if (ad2) $(".community-ads .adbox:nth-child("+ad2+")").insertAfter($(".sidebar .content .community-item")[pos2]);
 }
 
-
+// Load sidebar using Ajax
 function loadCommunity()
 {
 	$.get('/mobile/community', function(data) {
